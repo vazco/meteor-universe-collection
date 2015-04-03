e7331c6028ac1f46d4f4eb18d57b793e33198698
cristo-rabani <cristo.rabani@gmail.com>
2015-04-03 16:13:57 +0200

    up

--- package.js
+++ package.js
@@ -1,18 +1,17 @@
 Package.describe({
     summary: 'Collections with helpers on document, prototyping own classes of doc. Users with helpers. Saving doc',
     name: 'vazco:universe-collection',
-    version: '1.0.5',
+    version: '1.0.6',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
 Package.on_use(function (api) {
     api.versionsFrom(['METEOR@1.0.4']);
-    api.use(['templating', 'ui', 'blaze'], 'client');
     api.use([
         'underscore',
         'accounts-base',
         'mongo',
-        'vazco:universe-utilities@1.0.3'
+        'vazco:universe-utilities@1.0.4'
     ], ['client', 'server']);
 
     api.use(['matb33:collection-hooks@0.7.11'], ['client', 'server'], {weak: true});
