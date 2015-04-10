6c2fc23092aa0bd69744b4cf786aa616c10aaec4
cristo-rabani <cristo.rabani@gmail.com>
2015-04-10 17:56:41 +0200

    up package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: 'Collections with helpers on document, prototyping own classes of doc. Users with helpers. Saving doc',
     name: 'vazco:universe-collection',
-    version: '1.1.5',
+    version: '1.1.6',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
