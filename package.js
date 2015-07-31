f64550699e9ada67ab667334ab654fedf303969d
cristo-rabani <cristo.rabani@gmail.com>
2015-07-31 16:57:23 +0200

    meteor methods on collections

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: 'Collections with helpers on document, prototyping own classes of doc. Users with helpers. Saving doc',
     name: 'vazco:universe-collection',
-    version: '1.2.8',
+    version: '1.5.0',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
