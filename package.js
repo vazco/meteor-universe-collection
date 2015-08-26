733cb72d4e9be1dea06c6a08943d95a115b89806
Radosław Miernik <radekmie@gmail.com>
2015-08-26 11:56:28 +0200

    version up

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: 'Collections with helpers on document, prototyping own classes of doc. Users with helpers. Saving doc',
     name: 'vazco:universe-collection',
-    version: '1.8.7',
+    version: '1.8.8',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
