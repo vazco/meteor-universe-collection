4e95a2a2e7b6cf204b9d14b898627da9b24b1d25
cristo-rabani <cristo.rabani@gmail.com>
2015-10-17 08:32:56 +0200

    proxy

--- package.js
+++ package.js
@@ -1,41 +1,10 @@
 Package.describe({
     summary: 'Collections with helpers on document, prototyping own classes of doc. Users with helpers. Saving doc',
     name: 'vazco:universe-collection',
-    version: '1.8.8',
+    version: '2.0.0',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom(['METEOR@1.1.0.3']);
-    api.use([
-        'ejson',
-        'check',
-        'underscore',
-        'accounts-base',
-        'mongo',
-        'minimongo',
-        'vazco:universe-utilities@1.1.5',
-        'aldeed:simple-schema@1.3.2'
-    ]);
-
-    api.imply('aldeed:simple-schema');
-
-    api.use(['matb33:collection-hooks@0.7.13', 'insecure@1.0.0'], {weak: true});
-
-    api.addFiles([
-        'lib/UniCollection.js',
-        'lib/UniCollectionValidators.js',
-        'lib/UniCollectionSS.js',
-        'lib/UniMethods.js',
-        'lib/docPrototypes/UniDoc.js',
-        'lib/UniUsers.js',
-        'lib/docPrototypes/UniUser.js',
-        'lib/BasicSchema.js',
-        'lib/UniSecure.js'
-    ]);
-
-    api.export([
-        'UniCollection',
-        'UniUsers'
-    ]);
+    api.imply('universe:collection@2.0.0');
 });
