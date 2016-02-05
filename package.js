0d1e8610b0eac76ee99f053da6cf43755731cc65
Mikołaj Ziemski <mikolaj.ziemski@vazco.eu>
2016-02-05 15:16:27 +0100

    don't cut 'options' argument (on insert)

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     summary: 'Remote method from doc & collection, helpers on document/user, own doc classes, Multischemas, Hooks',
     name: 'universe:collection',
-    version: '2.0.8',
+    version: '2.0.9',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
