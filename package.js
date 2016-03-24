3460fabb7eea7bdfa7f1508ba9cead710e1ee779
cristo-rabani <cristo.rabani@gmail.com>
2016-03-24 17:17:44 +0100

    fix for ensureUsers

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     summary: 'Remote method from doc & collection, helpers on document/user, own doc classes, Multischemas, Hooks',
     name: 'universe:collection',
-    version: '2.2.1',
+    version: '2.2.2',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
