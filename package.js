ddf44e78656a8e0d541c175ef93997bdfda3bbc9
kamilwojdylo <kamil.wojdylo@wp.pl>
2016-04-13 16:54:09 +0200

    Update package.js

--- package.js
+++ package.js
@@ -18,7 +18,7 @@ Package.onUse(function (api) {
         'underscore',
         'mongo',
         'minimongo',
-        'universe:utilities@2.2.2',
+        'universe:utilities@2.3.2',
         'aldeed:simple-schema@1.5.3'
     ]);
 
