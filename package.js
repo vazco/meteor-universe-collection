326ba60c86635ef23d0a16aacccdf6a27ca95660
Cristo <cristo.rabani@gmail.com>
2016-04-16 11:25:09 +0200

    Update package.js

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     summary: 'Remote method from doc & collection, helpers on document/user, own doc classes, Multischemas, Hooks',
     name: 'universe:collection',
-    version: '2.3.4',
+    version: '2.3.6',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
