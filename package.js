36cbe4750d3dd9900329263acd3c975afe306f73
cristo-rabani <cristo.rabani@gmail.com>
2016-05-20 18:17:37 +0200

    fix for arr

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     summary: 'Remote method from doc & collection, helpers on document/user, own doc classes, Multischemas, Hooks',
     name: 'universe:collection',
-    version: '2.3.8',
+    version: '2.3.9',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
