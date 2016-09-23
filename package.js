a8264b0008bdc35b51987694d32a401942fb4c2d
Mikołaj Ziemski <mikolaj.ziemski@vazco.eu>
2016-09-23 18:03:05 +0200

    fix

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     summary: 'Remote method from doc & collection, helpers on document/user, own doc classes, Multischemas, Hooks',
     name: 'universe:collection',
-    version: '2.6.0',
+    version: '2.6.1',
     git: 'https://github.com/vazco/meteor-universe-collection'
 });
 
